dafd00302688926ea710948b8cb8be68104a5188
guinokali <cesar.rodriguezgo@cun.edu.co>
2020-02-05 16:39:34 -0500

    agregamos la aplicacion test Flask

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+def suma (a,b) :
+	return a+b
+
+
+
 @app.route('/')
 def hello():
-    return "Hola mundo"
+	res = suma(3,2)
+	return "Hola mundo %s " % (res)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
